d9c6fae0ff758cb177c8a2fd0ebbec613de44ae6
Martin Rasmussen <mra@trifork.com>
2020-03-06 18:47:24 +0100

    Update UI to use new backend

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,38 +2,59 @@ import React, { useState, useEffect } from "react"
 import "./App.css"
 import { animated, useTransition } from "react-spring"
 
+type Result = {
+  score: number | undefined
+  region: string | undefined
+}
+
 const pages = [
   ({ style }: any) => (
     <animated.div style={{ ...style, background: "lightblue" }}>
       Hold on...
     </animated.div>
   ),
-  ({ style, score }: any) => (
+  ({ style, score, region }: any) => (
     <animated.div style={{ ...style, background: "lightgreen" }}>
-      The corona score is {score}
+      <small>The corona trend score in {region} is</small>
+      <h1>{score.toFixed(2)}</h1>
     </animated.div>
   ),
-  ({ style, score }: any) => (
+  ({ style, score, region }: any) => (
     <animated.div style={{ ...style, background: "lightpink" }}>
-      The corona score is {score}
+      <small>The corona trend score in {region} is</small>
+      <h1>{score.toFixed(2)}</h1>
     </animated.div>
   )
 ]
 
 export const App = () => {
   const [score, setScore] = useState(0)
+  const [region, setRegion] = useState("")
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(location => {
       const { coords } = location
+      startScoreFetcher(coords)
+    })
+  }, [])
 
+  const startScoreFetcher = (coords: {
+    latitude: number
+    longitude: number
+  }) => {
+    setTimeout(() => {
       fetch(
         `http://localhost:8800/?lat=${coords.latitude}&long=${coords.longitude}`
       ).then(res => {
-        res.json().then(json => setScore(json.score))
+        res.json().then(json => {
+          const result = json as Result
+          setRegion(result.region || "")
+          setScore(result.score || 0)
+          startScoreFetcher(coords)
+        })
       })
-    })
-  }, [])
+    }, 5000)
+  }
 
   const transitions = useTransition(score, p => p, {
     from: { opacity: 0 },
@@ -51,7 +72,7 @@ export const App = () => {
           Page = pages[2]
         }
 
-        return <Page key={key} style={props} score={item} />
+        return <Page key={key} style={props} score={item} region={region} />
       })}
     </div>
   )
